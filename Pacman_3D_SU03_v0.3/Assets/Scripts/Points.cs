dd74d841db211d45d2f9f7ade8971ceacff3de17
luciferwez <o.lucifer.lindell@gmail.com>
2015-11-19 20:52:36 +0100

    Poäng-script

---
+++ Pacman_3D_SU03_v0.3/Assets/Scripts/Points.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Points : MonoBehaviour 
+{
+	
+	public GameObject Point;		  
+
+
+	void Update () 
+	{
+							
+	}
+
+	void OnTriggerEnter(Collider other)						
+	{														
+		if (other.tag == "Pacman")							
+		{
+			Destroy(Point);
+		}
+	}
+}
